b62547b3963bf6087c1ae79c9cb67dbd348ed8e5
tokane888 <tokane888@gmail.com>
2025-06-08 19:37:37 +0900

    refactor: .sh linter指摘対応

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -4,8 +4,8 @@ USER=$(whoami)
 
 # hostの~/.ssh​が/tmp/.sshへマウントされているので、それをHOMEディレクトリへコピー。
 # 直接HOMEディレクトリにマウントしようとすると既存のものがあり失敗するため
-cp -r /tmp/.ssh $HOME
-chown -R $USER:$USER $HOME/.ssh
-chmod 600 $HOME/.ssh/*
+cp -r /tmp/.ssh "${HOME}"
+chown -R "${USER}":"${USER}" "${HOME}"/.ssh
+chmod 600 "${HOME}"/.ssh/*
 
 sudo chown -R vscode:golang /usr/local/go /go/pkg /go/bin /go/src
